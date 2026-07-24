975ec2d4418843420125c3ddea2d650e55001c26
Nico <niyongsheng@outlook.com>
2026-07-24 12:10:30 +0800

    moler: update sha256 for v0.1.2 (rebuild)

--- Casks/moler.rb
+++ Casks/moler.rb
@@ -1,6 +1,6 @@
 cask "moler" do
   version "0.1.2"
-  sha256 "c723796d104753f67ce055b42c8534aba2a78dc97c3927999e6af28707686832"
+  sha256 "f756cf288a722a49e00bd6771f7ca3f96d47a511741c5b928c2a8ccc18c06f42"
 
   url "https://github.com/niyongsheng/moler/releases/download/v#{version}/Moler-#{version}.dmg"
   name "Moler"
